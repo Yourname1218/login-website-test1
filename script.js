0c5871e049b59e81ebf11ec3cfd1729258b74609
Yourname1218 <101972412+Yourname1218@users.noreply.github.com>
2023-10-23 17:41:18 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -54,3 +54,57 @@ iconClose.addEventListener('click', ()=>{
     wrapper.classList.remove('active-student');
     wrapper.classList.remove('active-teacher');
 });
+
+
+//-------------------以下是驗證三次密碼鎖定功能----------------------------------------- 
+
+var wrongAttempts = 0;
+var correctPassword = '123456'; // 正確的密碼
+
+function checkPassword() {
+    var inputpwd = document.getElementById('pwd');
+    var wrongtext = document.getElementById('wrongtext');
+    
+    if (inputpwd.value !== correctPassword) {
+        wrongAttempts++;
+        if (wrongAttempts >= 3) {
+            inputpwd.disabled = true;
+            alert('You have made an incorrect entry three times. Please try again in 30 seconds.');
+            setTimeout(function() {
+                inputpwd.disabled = false;
+                wrongtext.innerHTML = '';
+                wrongAttempts = 0;
+            }, 30000);
+        } else {
+            alert('Password incorrect, please try it again');
+            message.innerHTML = 'Format error, please enter again';
+        }
+    }
+    inputpwd.value = '';
+}
+
+// -----------------------以下是驗證email是否正確(使用正規表達式)----------------------------
+
+function checkEmail() {
+    var intemail = document.getElementById('inputemail');
+    var message = document.getElementById('mesemail');
+    
+    if (intemail.validity.patternMismatch) {
+        alert('Format error, please enter again');
+        return false;  // 返回 false 表示電子郵件地址不符合規定
+    } else {
+        message.innerHTML = '';
+        return true;  // 返回 true 表示電子郵件地址符合規定
+    }
+}
+
+// -------------------------以下是將password與email驗證改成同步執行-------------------------------
+
+function checkBoth() {
+    if (checkEmail()) {
+        checkPassword();
+    }
+}
+
+document.getElementById('loginButton').onclick = checkBoth;
+document.getElementById('signup').onclick = checkEmail;
